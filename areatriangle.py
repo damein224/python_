5f0d6ebee76b32dd79f0c52184fb04a2c18ad166
damien224 <paxan.se@gmail.com>
2024-03-06 21:40:24 +0530

    commit 3

---
+++ areatriangle.py
@@ -0,0 +1,5 @@
+height = float(input("enter the height of the triangle: "))
+base =  float(input("enter the base of the triangle: "))
+
+area =(1/2)*height*base
+print("the area of the traingle is ",area)
---
+++ squareroot.py
@@ -0,0 +1,3 @@
+num = int(input("enter a number: "))
+sr = num**(1/2)
+print("the squareroot of the number is: ",sr)
